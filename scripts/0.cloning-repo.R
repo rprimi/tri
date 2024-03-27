bbf316e308cd4ea652b48a96db57324df327e9e8
Ana Crispim <anacarlacrispim@gmail.com>
2024-03-27 14:40:38 -0300

    add-script-cloning-repo

---
+++ scripts/0.cloning-repo.R
@@ -0,0 +1,14 @@
+# No libraries needed
+# Requirements: GitHub account and Git installed 
+# Tutorial to connect Git in RStudio: https://github.com/anacarlac/biblioteca-codigos
+
+# Source: https://github.com/rstats-tln/fork-and-clone-repo
+
+# In summary: 
+# 1) Fork repository into personal account (https://github.com/rprimi/tri)
+# 2) Go to the repository in your account and click on "Code" (green button)
+# 3) Copy the web link from the https tab
+# 4) Open R Studio > File > New Project > Version control > Git > 
+# 5) Add the link https://github.com/rprimi/tri in the repository box 
+# 6) Adjust the path in the "Create project as subrepository of" box to the path you want in your computer
+# 7) Click on "Create repository" and 
