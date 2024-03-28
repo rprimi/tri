29d213b99871ab15a206adfbb2037072307e00d9
Ana Crispim <anacarlacrispim@gmail.com>
2024-03-28 08:48:10 -0300

    update-cloning-repo-script

--- scripts/0.cloning-repo.R
+++ scripts/0.cloning-repo.R
@@ -8,7 +8,9 @@
 # 1) Fork repository into personal account (https://github.com/rprimi/tri)
 # 2) Go to the repository in your account and click on "Code" (green button)
 # 3) Copy the web link from the https tab
-# 4) Open R Studio > File > New Project > Version control > Git > 
-# 5) Add the link https://github.com/rprimi/tri in the repository box 
+# 4) Open R Studio > File > New Project > Version control > Git 
+# 5) Add the link https://github.com/rprimi/tri in the repo box 
 # 6) Adjust the path in the "Create project as subrepository of" box to the path you want in your computer
-# 7) Click on "Create repository" and 
+# 7) Click on "Create repository" 
+
+# After creating the repository, click "Pull" in the Git tab of the RStudio interface to update the content from rprimi's account.
